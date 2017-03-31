29c0b607dc2bc6a308c5e0b38764e4cdde4f3714
danlauby <dmlauby@gmail.com>
2017-03-31 09:06:11 -0700

    Hard code instantiated entries in root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Entry } from './entry.model';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Check YoSelf!';
+  selectedEntry = null;
+
+  masterEntryList: Entry[] = [
+    new Entry('Pizza', 'Had pepperoni and cheese toppings. I ate a whole XL myself, yum.', 2543),
+    new Entry('Lime', 'Enjoyed a wedge of lime in my Corona. Feeling good about my diet!', 28),
+    new Entry('Top Ramen', 'One helping and was light on the seasoning. A well rounded diet for a student.', 654),
+  ];
 }
